7efb97f2a7c0c04caa18e6a92a314e664427550a
Vraj Mehta <vrajjmehta@gmail.com>
2019-09-19 18:02:00 +1000

    Update LoginForm.cs

--- WindowsForms_TextEditor/LoginForm.cs
+++ WindowsForms_TextEditor/LoginForm.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,45 @@ namespace WindowsForms_TextEditor
 {
     public partial class LoginForm : Form
     {
+        bool loginflag;
         public LoginForm()
         {
+            loginflag = false;
             InitializeComponent();
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void login_Click(object sender, EventArgs e)
+        {
+            string[] logindata = File.ReadAllLines("login.txt");
+
+            foreach(string set in logindata)
+            {
+                string[] splits = set.Split(',');
+                if(splits[0]==username_box.Text && splits[1] == password_Box.Text)
+                {
+                    DialogResult result = MessageBox.Show("Login Successful!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    loginflag = true;
+                    if (result == DialogResult.OK)
+                    {
+                        this.Hide();
+                    }
+                    break;
+                }
+            }
+            if (loginflag == false)
+            {
+                 DialogResult result = MessageBox.Show("Login Unsuccessful", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (result == DialogResult.OK)
+                {
+                    username_box.Clear();
+                    password_Box.Clear();
+                }
+            }
         }
     }
 }
